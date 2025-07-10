59ed330aae1191066ab268ec88207d862d4998f1
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2025-07-10 16:28:17 +0100

    add uuid and organise offers on backend

--- teleLegal-back-end/expressRoutes.js
+++ teleLegal-back-end/expressRoutes.js
@@ -2,15 +2,26 @@
 const app = require("./server").app;
 const jwt = require("jsonwebtoken");
 const linkSecret = "jh54645h6b45j6hj45b";
+const { v4: uuidv4 } = require("uuid");
+
+const professionalAppointments = [];
+
+app.set("professionalAppointments", professionalAppointments);
 
 //this route is for US. In production a receptionist would send it out. We will print it out and paste it in.
 //It will drop us on our React site with the right info for CLIENT1 to make an offer
 app.get("/user-link", (req, res) => {
+  const uuid = uuidv4();
+
   //data for the end-user's appt
   const apptData = {
     professionalsFullName: "Anna Trubnikova",
     apptDate: Date.now() + 500000,
+    uuid,
   };
+
+  professionalAppointments.push(apptData);
+
   //we need to encode this data in a token so it can be added to url
   const token = jwt.sign(apptData, linkSecret);
   res.send("https://localhost:3002/join-video?token=" + token);
@@ -24,4 +35,5 @@ app.post("/validate-link", (req, res) => {
   const decodedData = jwt.verify(token, linkSecret);
   //send the decoded data (our object) back to the front end
   res.json(decodedData);
+  console.log(professionalAppointments);
 });
--- teleLegal-back-end/socketServer.js
+++ teleLegal-back-end/socketServer.js
@@ -1,6 +1,24 @@
 //all socketServer stuff happens here
 const io = require("./server").io;
+const app = require("./server").app;
+
+// const professionalAppointments = app.get("professionalAppointments");
+
+const allKnownOffers = {
+  //uniqueID - key
+  //offer
+  //professionalsFullNmae
+  //clientName
+  //apptDate
+  //offererIceCandidates
+  //answer
+  //answerIceCandidates
+};
 
 io.on("connection", (socket) => {
   console.log(socket.id, "has connected");
+  socket.on("newOffer", ({ offer, apptData }) => {
+    //ofer = sdp/type, apptInfo has the uuid that we can add to allKnownOffers
+    //so that, the professional can find EXACTLY the right allKnownOffers
+  });
 });

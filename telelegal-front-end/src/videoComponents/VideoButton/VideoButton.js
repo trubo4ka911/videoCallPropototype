0a3bc452ee8d8589bcdd03a18f18737bc6af72b5
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2025-07-10 11:35:57 +0100

    display local video inputs (camera options)

--- telelegal-front-end/src/videoComponents/VideoButton/VideoButton.js
+++ telelegal-front-end/src/videoComponents/VideoButton/VideoButton.js
@@ -2,12 +2,42 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import startLocalVideoStream from "./startLocalVideoStream";
 import updateCallStatus from "../../redux/actions/updateCallStatus";
+import getDevices from "../../webRTCutilities/getDevices";
 
 const VideoButton = ({ smallFeedEl }) => {
   const dispatch = useDispatch();
   const callStatus = useSelector((state) => state.callStatus);
   const streams = useSelector((state) => state.streams);
   const [pendingUpdate, setPendingUpdate] = useState(false);
+  const [caretOpen, setCaretOpen] = useState(false);
+  const [videoDeviceList, setVideoDeviceList] = useState([]);
+
+  const changeVideoDevice = () => {};
+
+  const DropDown = () => {
+    return (
+      <div className="caret-dropdown" style={{ top: "-25px" }}>
+        <select defaultValue={1} onChange={changeVideoDevice}>
+          {videoDeviceList.map((vd) => (
+            <option key={vd.deviceId} value={vd.deviceId}>
+              {vd.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  };
+  useEffect(() => {
+    const getDevicesAsync = async () => {
+      if (caretOpen) {
+        //check for video devices
+        const devices = await getDevices();
+        console.log(devices.videoDevices);
+        setVideoDeviceList(devices.videoDevices);
+      }
+    };
+    getDevicesAsync();
+  }, [caretOpen]);
 
   const startStopVideo = () => {
     // console.log("check");
@@ -38,13 +68,17 @@ const VideoButton = ({ smallFeedEl }) => {
   }, [pendingUpdate, callStatus.haveMedia]);
   return (
     <div className="button-wrapper video-button d-inline-block">
-      <i className="fa fa-caret-up choose-video"></i>
+      <i
+        className="fa fa-caret-up choose-video"
+        onClick={() => setCaretOpen(!caretOpen)}
+      ></i>
       <div className="button camera" onClick={startStopVideo}>
         <i className="fa fa-video"></i>
         <div className="btn-text">
           {callStatus.video === "enabled" ? "Stop" : "Start"} Video
         </div>
       </div>
+      {caretOpen ? <DropDown /> : <></>}
     </div>
   );
 };
